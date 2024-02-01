5dd9dc1649bc0c23ef3a55727dffa274c7f1af4d
crav99 <allanvargas000@gmail.com>
2024-02-01 09:22:34 -0600

    Create header section

---
+++ Netflix Clone/Controllers/Core/SearchViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SearchViewController.swift
+//  Netflix Clone
+//
+//  Created by Allan Vargas Zumbado on 31/1/24.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+    }
+
+}
---
+++ Netflix Clone/Controllers/Core/UpcomingViewController.swift
@@ -0,0 +1,18 @@
+//
+//  UpcomingViewController.swift
+//  Netflix Clone
+//
+//  Created by Allan Vargas Zumbado on 31/1/24.
+//
+
+import UIKit
+
+class UpcomingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+    }
+
+}
